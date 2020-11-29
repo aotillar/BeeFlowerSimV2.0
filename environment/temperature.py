0f2c38f13e898f246ebec3057da5237326398715
aotillar <aotillarr@gmail.com>
2020-11-28 20:36:45 -0700

    Updated the temperature.py with old code.

---
+++ environment/temperature.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+
+def temp_north_day_high(thigh, tlow, day):
+    """This function uses a trigonometric function and transposes it
+    to fit the thigh, tlow. This model assumes that temperature changes
+    uniformly throughout the year according to a standard wave function.
+    Once the function is transposed, it then uses day as an x variable
+    to get the according y value(temperature) for that particular day"""
+    x = np.linspace(0, 365, 365)
+    a = (thigh - tlow) / 2
+    d = (thigh + tlow) / 2
+    y = a * np.cos((2 * np.pi / 365) * (x - 182.5)) + d
+    if day:
+        return y[day]
+    if day == 0:
+        return y[1]
+    else:
+        return y
+
+
+def temp_north_day_low(thigh, tlow, day):
+    x = np.linspace(0, 365, 365)
+    a = (thigh - tlow) / 2
+    d = (thigh + tlow) / 2
+    y = a * np.cos((2 * np.pi / 365) * (x - 182.5)) + d
+    if day:
+        return y[day]
+    if day == 0:
+        return y[1]
+    else:
+        return y
+
+
+def temp_south_day_high(thigh, tlow, day=None):
+    x = np.linspace(0, 365, 365)
+    a = (thigh - tlow) / 2
+    d = (thigh + tlow) / 2
+    y = a * -np.cos((2 * np.pi / 365) * (x - 182.5)) + d
+    if day:
+        return y[day]
+    else:
+        return y
+
+
+def temp_south_day_low(thigh, tlow, day=None):
+    x = np.linspace(0, 365, 365)
+    a = (thigh - tlow) / 2
+    d = (thigh + tlow) / 2
+    y = a * -np.cos((2 * np.pi / 365) * (x - 182.5)) + d
+    if day:
+        return y[day]
+    else:
+        return y
+
+
+def temp_hour(thigh, tlow, hour=None):
+    """This function uses a trigonometric function and transposes it
+        to fit the thigh, tlow. This model assumes that temperature changes
+        uniformly throughout the year according to a standard wave function.
+        Once the function is transposed, it then uses hour as an x variable
+        to get the according y value(temperature) for that particular hour"""
+    x = np.linspace(0, 24, 24)
+    a = (thigh - tlow) / 2
+    d = (thigh + tlow) / 2
+    y = a * np.cos((2 * np.pi / 24) * (x - 182.5)) + d
+    return y[hour]
+
+
+def random_daily_temperature(high_lrg, high_lw, low_lrg, low_lw, day):
+    """High and Low values are for Yearly All Time high, and Yearly all time low.
+    This function assumes a bell curve for yearly average temperature."""
+    y1 = temp_north_day_high(high_lrg, high_lw, day)
+    y2 = temp_north_day_low(low_lrg, low_lw, day)
+    y_average = (y1 + y2) / 2
+    return np.random.normal(y_average, 10)
+
